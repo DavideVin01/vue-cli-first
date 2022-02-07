50e9dd1862e35838e5f7a02f3a92277907cd7989
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-02-07 15:55:54 +0100

    Removes "HelloWorld.vue" | Replaces with title.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <title />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import title from "@components/title.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    title,
   },
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
 </style>
---
+++ src/components/title.vue
@@ -0,0 +1,12 @@
+<template>
+  <title><h1>La mia prima app con Vue CLI!</h1></title>
+</template>
+
+<script>
+export default {
+  name: "title",
+};
+</script>
+
+<style>
+</style>
