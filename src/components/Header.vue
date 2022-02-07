db244cb702a3f9227cc574af239be84eacdfd18f
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-02-07 16:48:52 +0100

    Adds more style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,8 @@
 <template>
   <header class="shadow">
-    <h1 class="scale-up-center">La mia prima app con Vue CLI!</h1>
+    <h1 id="title" class="scale-up-center">
+      La mia prima app con <span id="text-green">Vue CLI!</span>
+    </h1>
   </header>
 </template>
 
@@ -13,5 +15,13 @@ export default {
 <style>
 header {
   background-color: #43b885;
+}
+
+#title {
+  color: #36495e;
+}
+
+#text-green {
+  color: #98ffd2;
 }
 </style>
