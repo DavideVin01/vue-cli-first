78cf4ce7f6f14e6d1f7b12b8278c2bba02e5a0ba
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-02-07 16:22:52 +0100

    Adds Hero Component

---
+++ src/components/Hero.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <img src="@/assets/Jumbotron.png" alt="Jumbotron" />
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style>
+</style>
